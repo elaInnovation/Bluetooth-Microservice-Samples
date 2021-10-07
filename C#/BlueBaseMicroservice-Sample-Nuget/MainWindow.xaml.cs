f24136c99ca124f3d09a0162b35f3dff32ff5b67
matbuster <abet.mathieu@gmail.com>
2021-10-07 18:13:41 +0200

    update with the first c# sample

---
+++ C#/BlueBaseMicroservice-Sample-Nuget/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using BlueBaseMicroservice_Sample.Configuration;
+using BlueBaseMicroservice_Sample.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlueBaseMicroservice_Sample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /** \brief constructor */
+        public MainWindow()
+        {
+            InitializeComponent();
+            initializeInternalComponents();
+            localizeComponents();
+        }
+
+        #region implement IuserControlBase interface
+        public void initializeInternalComponents()
+        {
+            TabItem settingsTab = new TabItem();
+            //
+            if (true == ElaMicroConfiguration.getInstance().Settings?.BluetoothConfiguration?.UserAllowed)
+            {
+                TabItem blueTab = new TabItem();
+                blueTab.Header = ElaMicroConfiguration.getInstance().Settings?.BluetoothConfiguration.GrpcServiceName;
+                blueTab.Content = new MicroBlueManager() { Tag = ElaMicroConfiguration.getInstance().Settings?.BluetoothConfiguration.GrpcServiceName };
+                this.tabMain.Items.Add(blueTab);
+            }
+            //
+        }
+
+        public void localizeComponents()
+        {
+
+        }
+        #endregion
+    }
+}
