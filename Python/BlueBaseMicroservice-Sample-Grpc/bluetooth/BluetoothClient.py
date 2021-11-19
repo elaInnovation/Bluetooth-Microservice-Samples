d5aabf8b09e8ac3322457db54667085ead97b913
matbuster <abet.mathieu@gmail.com>
2021-11-19 16:24:37 +0100

    update the login information

--- Python/BlueBaseMicroservice-Sample-Grpc/bluetooth/BluetoothClient.py
+++ Python/BlueBaseMicroservice-Sample-Grpc/bluetooth/BluetoothClient.py
@@ -26,10 +26,8 @@ class BluetoothClient():
             filter_only_connectable=False,
             filter_type=ElaBluetoothCommon_pb2.ElaBluetoothFilter.FilterType.No_Option)
             
-    #ELA_LOGIN = "elainnovation"
-    #ELA_CERTIFICATE = "{\"data\":\"4e2aaabf6d91030218dc624821d38a6b2bbbfd329e8543af1ce1882589aa3e7ad640bc03e91033ddce426e7deba6548536de0f647760635090eaed9b1264701f4cf498c14007daec747f59ff6b5b5ee88d7563b49455fa51d45e9c89ee8ac03ad27a83e307b5301aa508bbad78ab512759c3ad95ee24187e097c797c638d105d\",\"sign\":\"bdee89baafa0a6f2db0206f16e0d14a657d8d6e10221571f9b998ca4c47d3b0ff5b8de770d046d52b1fa271908c61c0df6e4db83c3c217a233a8e74c5524a85f9affdd6d522588a167a0e7936e857f77c85c1da61f98a0461c8e6c80b836142a1f88115f74fdfb4de80b497bbe40a996b2172462482466493c636abe4099e500\"}"
-    ELA_LOGIN = "admin"
-    ELA_CERTIFICATE = "ElaSuperUser42"
+    ELA_LOGIN = "ElaTrustedUser"
+    ELA_CERTIFICATE = "E1999l42A"
         
     def __init__(self):
         """
